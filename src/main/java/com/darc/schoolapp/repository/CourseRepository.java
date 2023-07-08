3ea423303b26430f0d5867648d887b974951c2de
Darc99 <lennyyhugoh@gmail.com>
2023-07-08 22:36:37 +0100

    added course and materials

---
+++ src/main/java/com/darc/schoolapp/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.darc.schoolapp.repository;
+
+import com.darc.schoolapp.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
